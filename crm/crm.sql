46f1ee5c7f6290ae770d3d2589614d6baaad0da4
EasonLee2 <eason.lee@kellogg.northwestern.edu>
2022-04-21 00:32:44 +0000

    most updated version

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,42 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
 
 -- CREATE TABLES
+
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT
+);
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  address TEXT,
+  phone_number TEXT
+);
+
+CREATE TABLE activites (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type TEXT,
+  time TEXT,
+  note TEXT,
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+);
+
+
+
+
